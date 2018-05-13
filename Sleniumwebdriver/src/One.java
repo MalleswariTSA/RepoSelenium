9bb2b456133e42e15f4bbaded8d843378c88773a
LENOVO <LENOVO@SHRI>
2018-05-13 10:22:41 +0530

    Newproj

--- Sleniumwebdriver/src/One.java
+++ Sleniumwebdriver/src/One.java
@@ -3,10 +3,9 @@ public class One {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("Hello World");
+		System.out.println("hello world");
 		System.out.println("Welcome to GIT");
-		
+
 	}
 
 }
